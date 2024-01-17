83f2223f68fd671260b67320088abf5b1ce6905a
stlee321 <lstdess321@gmail.com>
2024-01-17 11:07:54 +0900

    test profile 추가

--- app/src/test/java/me/stlee321/instatube/app/AppApplicationTest.java
+++ app/src/test/java/me/stlee321/instatube/app/AppApplicationTest.java
@@ -2,8 +2,10 @@ package me.stlee321.instatube.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("test")
 class AppApplicationTest {
 
     @Test
